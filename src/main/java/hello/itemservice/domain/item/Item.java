75fd4ce6f8c511c765be14a733b458691070a8b9
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-12-14 15:42:59 +0900

    feat: BeanValidation 에러 코드, 오브젝트 오류, 수정, groups 적용 로직

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,7 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
@@ -9,15 +10,18 @@ import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
-
+    @NotNull(groups = UpdateCheck.class) // 수정 요구사항.
     private Long id;
-    @NotBlank(message = "공백은 안됩니다.")
+
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull
-    @Range(min = 1000, max= 1000000)
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max= 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
-    @NotNull
-    @Max(9999)
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = {SaveCheck.class}) //수정 요구사항.
     private Integer quantity;
 
     public Item() {
